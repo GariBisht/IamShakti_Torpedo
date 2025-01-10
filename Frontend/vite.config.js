c1a22115a3f07a11629829b950926c6b286c6ab3
Garima Bisht <74089873+GariBisht@users.noreply.github.com>
2025-01-10 20:47:19 +0530

    add Sever

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 
-
+// For Avoid CORS Error while hitting an api 
 export default defineConfig({
-  plugins: [react()],
+//add server
+  server:{
+    proxy:{
+      '/api':'http://localhost:3000'
+    }
+  },
+ lugins: [react()],
+
 })
